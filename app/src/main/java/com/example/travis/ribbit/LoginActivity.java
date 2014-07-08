a9b4ad557933968480299847865fad5f3281e5d6
travisyim <troviss@hotmail.com>
2014-07-07 19:54:12 -0700

    End of "Adding Users Using Parse.com" - Added progress bar to login

--- app/src/main/java/com/example/travis/ribbit/LoginActivity.java
+++ app/src/main/java/com/example/travis/ribbit/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -27,6 +28,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_login);
 
         mUsername = (EditText) findViewById(R.id.editTextUsername);
@@ -52,9 +54,13 @@ public class LoginActivity extends Activity {
                 }
                 else {
                     // User inputted fields are valid
+                    setProgressBarIndeterminateVisibility(true);
+
                     ParseUser.logInInBackground(username, password, new LogInCallback() {
                         @Override
                         public void done(ParseUser parseUser, ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+
                             if (parseUser != null) {
                                 // Success!
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
--- app/src/main/java/com/example/travis/ribbit/SignUpActivity.java
+++ app/src/main/java/com/example/travis/ribbit/SignUpActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,6 +25,7 @@ public class SignUpActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_sign_up);
 
         mUsername = (EditText) findViewById(R.id.editTextUsername);
@@ -50,6 +52,8 @@ public class SignUpActivity extends Activity {
                 }
                 else {
                     // User inputted fields are valid
+                    setProgressBarIndeterminateVisibility(true);
+
                     ParseUser user = new ParseUser();
                     user.setUsername(username);
                     user.setPassword(password);
@@ -58,6 +62,8 @@ public class SignUpActivity extends Activity {
                     user.signUpInBackground(new SignUpCallback() {
                         @Override
                         public void done(ParseException e) {
+                            setProgressBarIndeterminateVisibility(false);
+
                             if (e == null) {
                                 // Success!
                                 Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
